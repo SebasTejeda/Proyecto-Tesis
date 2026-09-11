a9665ef09f1165e3333880a05764388d809623e7
Ubuntu <ubuntu@ip-172-31-30-99.ec2.internal>
2026-09-11 21:15:57 +0000

    Agregar dominio de Vercel al CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -41,7 +41,7 @@ app.add_middleware(SlowAPIMiddleware)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200", "http://127.0.0.1:4200"],
+    allow_origins=["http://localhost:4200", "http://127.0.0.1:4200", "https://proyecto-tesis-omega.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
